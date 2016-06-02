a25ca3f1157aa9039be244ea67e1c23c1aaeb904
Dan Amador <danamadorpe@gmail.com>
2016-06-02 14:23:33 -0500

    Added base skeleton for bot

--- telebot.py
+++ telebot.py
@@ -1,11 +1,34 @@
+#!/usr/bin/env python3
+
 import json
 
-from telegram.ext import *
-
-with open('settings.json') as settings:
-    data = json.load(settings)
-    updater = Updater(data["telegram_token"])
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 def test(bot, update):
     bot.sendMessage()
+
+
+def learn(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="aww yiss")
+
+
+def updates(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
+
+def initialize():
+    with open('settings.json') as settings:
+        data = json.load(settings)
+        updater = Updater(token=data["telegram_token"])
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler('learn', learn))
+    dispatcher.add_handler(MessageHandler([Filters.text], updates))
+
+    print("shit's working,yo %s") % data['telegram_token']
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    initialize()
