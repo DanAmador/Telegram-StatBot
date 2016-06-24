9c4fb50cc31493aeb000589cb940bed1d4afe4fa
Dominik Muhs <dominik.muhs@mailbox.tu-dresden.de>
2016-06-23 20:51:56 -0500

    Fix spacing and whitespace styles in telebot.py

--- telebot.py
+++ telebot.py
@@ -4,8 +4,9 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dbHelper import dbHelper
 
-
 db = dbHelper()
+
+
 def learn(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="aww yiss")
 
@@ -14,17 +15,16 @@ def updates(bot, update):
     db.messageInsert(update)
 
 
-def count(bot, update,args):
-    username,messages, words = db.count(update,args)
+def count(bot, update, args):
+    username, messages, words = db.count(update, args)
     sentMessage = "%s has sent  %s messages in this chat with a total of %s words" if args[0] != 'all' else "The chat %s has a total of %s messages with %s words"
-    results = sentMessage % (username,messages, words)
+    results = sentMessage % (username, messages, words)
     bot.sendMessage(chat_id=update.message.chat_id, text=results)
 
 
-def overall(bot,update):
+def overall(bot, update):
     stats = db.minMaxParse(update)
     bot.sendMessage(chat_id=update.message.chat_id, text=stats)
-
 
 
 def initialize():
@@ -35,7 +35,7 @@ def initialize():
 
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('learn', learn))
-    dispatcher.add_handler(CommandHandler('chatstats',count,pass_args=True))
+    dispatcher.add_handler(CommandHandler('chatstats', count, pass_args=True))
     dispatcher.add_handler(CommandHandler('overall', overall))
     dispatcher.add_handler(MessageHandler([Filters.text], updates))
 
@@ -45,7 +45,6 @@ def initialize():
 
     updater.start_polling()
     updater.idle()
-
 
 
 if __name__ == '__main__':
