684bcf5edb385abfa2c084b899650a7c844d825a
Dan Amador <danamadorpe@gmail.com>
2016-06-26 13:25:08 -0500

    Install and run scripts created

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #Install lua5
-apt-get install lua5.2
+apt-get install lua5.1
+apt-get install liblua5.1-0-dev
 #Lua Rocks
 git clone https://github.com/torch/distro.git ~/torch --recursive
 cd ~/torch; bash install-deps;
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mongod &
+python telebot.py &
